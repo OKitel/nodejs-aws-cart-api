8721d6172f8b74e1f9620a716c9c29065f6490c6
OKitel <kitelolga@gmail.com>
2024-07-31 10:30:28 +0200

    feat: add sql scripts for creating users and orders db tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TYPE cart_status AS ENUM ('OPEN', 'ORDERED');
 
 CREATE TABLE carts (
-   id UUID primary key default uuid_generate_v4(),
+   id UUID PRIMARY KEY default uuid_generate_v4(),
    user_id UUID NOT NULL,
    created_at DATE NOT NULL,
    updated_at DATE NOT NULL,
@@ -15,4 +15,25 @@ CREATE TABLE cart_items (
    product_id UUID NOT NULL,
    count INTEGER NOT NULL CHECK (count > 0),
    FOREIGN KEY (cart_id) REFERENCES carts(id)
+);
+
+CREATE TABLE users (
+  id UUID PRIMARY KEY default uuid_generate_v4(),
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE orders (
+  id UUID PRIMARY KEY default uuid_generate_v4(),
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
+  cart_id UUID NOT NULL REFERENCES carts(id) ON DELETE NO ACTION,
+  payment JSON,
+  delivery JSON,
+  comments TEXT,
+  status VARCHAR(10) DEFAULT 'OPEN' CHECK (status IN ('OPEN', 'NO PAYMENT', 'CONFIRMED', 'IN PROGRESS', 'SHIPPED', 'COMPLETED', 'CANCELLED')),
+  total NUMERIC(10, 2) NOT null,
+  FOREIGN KEY (cart_id) REFERENCES carts(id)
 );
